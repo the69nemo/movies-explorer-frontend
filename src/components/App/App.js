67c2c3cb4cd05aaa8a0cc1099e34c91140c51326
the69nemo <thestigmata@yandex.ru>
2022-06-07 21:39:56 +0300

    some route and page_404: done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Main from "../Main/Main";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import PageNotFound from "../PageNotFound/PageNotFound";
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="page">
-      <Header />
-      <Main />
-      <Footer />
+      {isLoggedIn && <Header />}
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+      {isLoggedIn && <Footer />}
     </div>
   );
 }
