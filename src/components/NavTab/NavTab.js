cd9fba2d95cee286a9aef99225c627dc4f6b6ae7
the69nemo <thestigmata@yandex.ru>
2022-06-07 16:15:51 +0300

    main_768px: done

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -5,9 +5,9 @@ function NavTab() {
   return (
     <section className='navtab'>
       <ul className='navtab__container'>
-        <li className='navtab__list'><a className='navtab__link' href='/'>О проекте</a></li>
-        <li className='navtab__list'><a className='navtab__link' href='/'>Технологии</a></li>
-        <li className='navtab__list'><a className='navtab__link' href='/'>Студент</a></li>
+        <li className='navtab__list'><a className='navtab__link' href='#about-project'>О проекте</a></li>
+        <li className='navtab__list'><a className='navtab__link' href='#techs'>Технологии</a></li>
+        <li className='navtab__list'><a className='navtab__link' href='#about-me'>Студент</a></li>
       </ul>
     </section>
    );
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -3,7 +3,7 @@ import "./Techs.css";
 
 function Techs() {
   return (
-    <section className="techs">
+    <section className="techs" id="techs">
       <div className="techs__container">
         <h2 className="techs__title">Технологии</h2>
         <div className="techs__line" />
