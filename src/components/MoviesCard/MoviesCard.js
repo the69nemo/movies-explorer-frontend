e698f8beec275d07d1cd5755ebce8ce803bf486e
the69nemo <thestigmata@yandex.ru>
2022-06-08 17:51:26 +0300

    path /movies: done

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './MoviesCard.css';
-import Cover  from '../../images/movie_pic_1.jpg'
+import React from "react";
+import "./MoviesCard.css";
 
-function MoviesCard ({ cardName, imgPath, time}) {
+function MoviesCard({ cardName, imgCover, time }) {
   return (
-    <section className='movies-card'>
-      <div className='movies-card__container'>
-        <div className='movies-card__text-wrapper'>
-          <h2 className='movies-card__title'>33 слова о дизайне</h2>
-          <p className='movies-card__time'>1ч 42м</p>
-          <button className='movies-card__like' type='button' />
+    <section className="movies-card">
+      <div className="movies-card__container">
+        <div className="movies-card__text-wrapper">
+          <div className="movies-card__text-container">
+            <h2 className="movies-card__title"> {cardName} </h2>
+            <p className="movies-card__time"> {time} </p>
+          </div>
+          <div className="movies-card__button-container">
+            <button className="movies-card__like-btn" type="button" />
+          </div>
         </div>
-        <img className='movies-card__img-cover' alt='кадр из фильма' src={Cover}/>
+        <img
+          className="movies-card__img-cover"
+          alt="кадр из фильма"
+          src={imgCover}
+        />
       </div>
     </section>
   );
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,15 +1,53 @@
 import React from "react";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
+import Cover1 from "../../images/movie_pic_1.jpg";
+import Cover2 from "../../images/movie_pic_2.jpg";
+import Cover3 from "../../images/movie_pic_3.jpg";
+import Cover4 from "../../images/movie_pic_4.jpg";
+import Cover5 from "../../images/movie_pic_5.jpg";
+import Cover6 from "../../images/movie_pic_6.jpg";
+import Cover7 from "../../images/movie_pic_7.jpg";
 
 function MoviesCardList() {
   return (
     <section className="movies-list">
       <div className="movies-list__container">
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
-        <MoviesCard />
+        <MoviesCard
+          cardName="33 слова о дизайне"
+          time="1ч 42м"
+          imgCover={Cover1}
+        />
+        <MoviesCard
+          cardName="Киноальманах «100 лет дизайна»"
+          time="1ч 42м"
+          imgCover={Cover2}
+        />
+        <MoviesCard
+          cardName="В погоне за Бенкси"
+          time="1ч 42м"
+          imgCover={Cover3}
+        />
+        <MoviesCard
+          cardName="Баския: Взрыв реальности"
+          time="1ч 42м"
+          imgCover={Cover4}
+        />
+        <MoviesCard
+          cardName="Бег это свобода"
+          time="1ч 42м"
+          imgCover={Cover5}
+        />
+        <MoviesCard
+          cardName="Книготорговцы"
+          time="1ч 42м"
+          imgCover={Cover6}
+        />
+        <MoviesCard
+          cardName="Когда я думаю о Германии ночью"
+          time="1ч 42м"
+          imgCover={Cover7}
+        />
       </div>
       <button type="button" className="movies-list__button">
         Еще
