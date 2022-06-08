c003738bb4c9dc8a0be3a1f4aa958fa1bb4a1ad0
the69nemo <thestigmata@yandex.ru>
2022-06-09 01:31:24 +0300

    route /signup: done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,29 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-import Main from "../Main/Main"
+import Main from "../Main/Main";
 import PageNotFound from "../PageNotFound/PageNotFound";
 import Movies from "../Movies/Movies";
 import SaveMovies from "../SavedMovies/SavedMovies";
-function App() {
+import Register from "../Register/Register";
 
+function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
     <div className="page">
       <Switch>
+        <Route path="/signup">
+          <Register />
+        </Route>
         <Route path="/" exact>
-          <Main
-            isLoggedIn={isLoggedIn}
-          />
+          <Main isLoggedIn={isLoggedIn} />
         </Route>
-        <Route path="/movies" >
-          <Movies
-            isLoggedIn={isLoggedIn}
-          />
+        <Route path="/movies">
+          <Movies isLoggedIn={isLoggedIn} />
         </Route>
-        <Route path="/saved-movies" >
-          <SaveMovies
-            isLoggedIn={isLoggedIn}
-          />
+        <Route path="/saved-movies">
+          <SaveMovies isLoggedIn={isLoggedIn} />
         </Route>
         <Route path="*">
           <PageNotFound />
---
+++ src/components/Register/Register.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Register.css";
+import Logo from "../../images/logo.svg";
+import { Link } from "react-router-dom";
+
+function Register() {
+  return (
+    <section className="register">
+      <div className="register__container">
+        <Link to="/">
+          <img className="register__logo" src={Logo} alt="логотип" />
+        </Link>
+        <h2 className="register__title">Добро пожаловать!</h2>
+        <form className="register__form">
+          <label className="register__label register__label_type_name">
+            Имя
+            <input
+              className="register__input register__input_type_name"
+              type="text"
+              required
+            />
+            <span
+              className="register__input-error"
+              id="name-input-error"
+            ></span>
+          </label>
+          <label className="register__label register__label_type_email">
+            E-mail
+            <input
+              className="register__input register__input_type_email"
+              type="email"
+              required
+            />
+            <span
+              className="register__input-error"
+              id="email-input-error"
+            ></span>
+          </label>
+          <label className="register__label register__label_type_password">
+            Пароль
+            <input
+              className="register__input register__input_type_password"
+              type="password"
+              required
+              minLength='2'
+            />
+            <span
+              className="register__input-error"
+              id="password-input-error"
+            ></span>
+          </label>
+        </form>
+        <button className="register__btn" type="button">
+          Зарегистрироваться
+        </button>
+        <p className="register__link-text">
+          Уже зарегистрированы?
+          <Link className="register__link" to="/signin">
+            Войти
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Register;
